9b9e1d5f484abba5a205f62089ed1b16c6c0eca9
Paul-Glanville <glanville49@googlemail.com>
2021-05-27 15:43:14 +0000

    added functions in app.py for login and register templates

--- app.py
+++ app.py
@@ -31,6 +31,53 @@ def search():
     return render_template("books.html", books=books)
 
 
+@app.route("/register", method=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+        
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful")
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
+
+
+@app.route("/login", method=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}", format(
+                    request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
+
+            else:
+                flash("incorrect username and/or password")
+                return redirect(url_for("login"))
+        
+        else:
+                flash("incorrect username and/or password")
+                return redirect(url_for("login"))
+    
+    return render_template("login.html")
+
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
